9d307d8bb9965b0d17d131f5c024bd575a8338b6
wilfred <wilfredkorir46@gmail.com>
2022-08-28 16:22:05 +0300

    phase-1-algorithms-palindrome

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+function reverse(word){
+  return word.split('').reverse().join('');
+}
+
 function isPalindrome(word) {
+  //SafeArrayQWE
   // Write your algorithm here
+  const reversedWord = reverse(word);
+  return word === reversedWord;
 }
 
 /* 
   Add your pseudocode here
+  when the word is the same as the word in reverse it should return true.
 */
 
 /*
   Add written explanation of your solution here
+  first we reverse the input, If the input is the same as the reversed input it,
+  returns true
+  else return false.
 */
 
 // You can run `node index.js` to view these console logs
@@ -19,7 +30,8 @@ if (require.main === module) {
   console.log("");
 
   console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  console.log(
+    "=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
